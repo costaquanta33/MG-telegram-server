bce3dee2272facdc2caa19965751060edfd31917
costaquanta33 <costaquanta@gmx.de>
2026-03-29 22:28:57 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // ═══════════════════════════════════════════════════════════
 
 const express = require('express');
-const fetch = require('node-fetch');
+
 const app = express();
 app.use(express.json());
 
